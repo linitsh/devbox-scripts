aaabd382b3e11ac320e3c901a3769ca57918c51d
Lin <169950434+linitsh@users.noreply.github.com>
2024-07-17 00:47:53 +0300

    Create devbox-install.sh

---
+++ devbox-install.sh
@@ -0,0 +1,5 @@
+apt-get update && apt-get install -y curl xz-utils
+curl -L https://nixos.org/nix/install | sh -s -- --daemon
+curl -fsSL https://get.jetify.com/devbox | bash -s -- -f
+echo 'eval "$(devbox global shellenv)"' >> ~/.bashrc
+eval "$(devbox global shellenv --recompute)"
