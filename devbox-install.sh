4a39489f1596ae11cf31355ab55610c52306fb5b
Lin <169950434+linitsh@users.noreply.github.com>
2024-07-17 00:55:05 +0300

    Update devbox-install.sh

--- devbox-install.sh
+++ devbox-install.sh
@@ -4,3 +4,4 @@ curl -L https://nixos.org/nix/install | sh -s -- --daemon
 curl -fsSL https://get.jetify.com/devbox | bash -s -- -f
 echo 'eval "$(devbox global shellenv)"' >> ~/.bashrc
 eval "$(devbox global shellenv --recompute)"
+echo "alias dbg='devbox global'" >> ~/.bashrc
