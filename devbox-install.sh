3d74474f069209d5c4f3113d5342253d07ad70e5
Lin <169950434+linitsh@users.noreply.github.com>
2024-07-17 18:45:30 +0300

    Update devbox-install.sh

--- devbox-install.sh
+++ devbox-install.sh
@@ -3,7 +3,9 @@ apt-get update && apt-get install -y curl xz-utils
 curl -L https://nixos.org/nix/install | sh -s -- --daemon
 curl -fsSL https://get.jetify.com/devbox | bash -s -- -f
 echo 'eval "$(devbox global shellenv)"' >> ~/.bashrc
+source ~/.bashrc
 eval "$(devbox global shellenv --recompute)"
+refresh-global
 echo "alias dbg='devbox global'" >> ~/.bashrc
 echo "alias db='devbox'" >> ~/.bashrc
 source ~/.bashrc
