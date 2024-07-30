40ffe25279bb3087de5d2c5b17af8517833bba05
Lin <Lin@gmail.com>
2024-07-30 16:32:44 +0300

    Updated

--- remote/bash/install/debian12-install-docker.sh
+++ remote/bash/install/debian12-install-docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# debian 12 init.d installation
+
 apt-get update -y
 apt-get install ca-certificates curl -y
 install -m 0755 -d /etc/apt/keyrings
@@ -10,10 +12,17 @@ echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
+
+# Install Docker
+apt-get update -y
 apt-get install fuse-overlayfs -y
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# docker fix
 sed -i 's/ulimit -Hn/# ulimit -n/g' /etc/init.d/docker
 
-echo "run: 'service docker start' for run docker daemoт"
+# clean?
+
+# init.d mode
+echo "run: 'service docker start' for run docker daemon"
 
--- remote/bash/install/devbox-install.sh
+++ remote/bash/install/devbox-install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+
+# test version
+
 apt-get update && apt-get install -y curl xz-utils
+
 curl -L https://nixos.org/nix/install | sh -s -- --daemon
 curl -fsSL https://get.jetify.com/devbox | bash -s -- -f
+
+# try global
 echo 'eval "$(devbox global shellenv)"' >> ~/.bashrc
 source ~/.bashrc
 eval "$(devbox global shellenv --recompute)"
 refresh-global
+
+# alias
 echo "alias dbg='devbox global'" >> ~/.bashrc
 echo "alias db='devbox'" >> ~/.bashrc
 source ~/.bashrc
